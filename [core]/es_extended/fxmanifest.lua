6ebfc3a99ff071e4a16e1f36d033f48716a08c65
Space V <ahcene.zidahnal@epitech.eu>
2023-05-02 03:55:05 +0200

    feat(manifest): Update fx_version to latest Cerulean version

--- [core]/es_extended/fxmanifest.lua
+++ [core]/es_extended/fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 
 game 'gta5'
 
@@ -42,7 +42,7 @@ client_scripts {
 	'client/modules/callback.lua',
 
 	'client/main.lua',
-	
+
 	'common/modules/*.lua',
 	'common/functions.lua',
 
--- [core]/esx_textui/fxmanifest.lua
+++ [core]/esx_textui/fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 game 'gta5'
 author 'ESX-Framework'
 version '1.9.4'
